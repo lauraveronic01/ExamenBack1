7b3d25db5641096fb3e6db91564b8208a70fedac
lauraveronic01 <lau.veronica01@gmail.com>
2024-04-18 20:39:52 -0300

    config-server en funcionamiento

--- movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -5,11 +5,13 @@ import com.dh.movieservice.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class MovieServiceApplication {
 
     public static void main(String[] args) {
@@ -23,10 +25,10 @@ public class MovieServiceApplication {
                 return;
             }
 
-            repository.save(new Movie(null, "Pelicula 1", "Terror", "www.netflix.com"));
-            repository.save(new Movie(null, "Pelicula 2", "Terror", "www.netflix.com"));
-            repository.save(new Movie(null, "Pelicula 3", "Comedia", "www.netflix.com"));
-            repository.save(new Movie(null, "Pelicula 4", "Ficcion", "www.netflix.com"));
+            repository.save(new Movie(1L, "Pelicula 1", "Terror", "www.netflix.com"));
+            repository.save(new Movie(2L, "Pelicula 2", "Terror", "www.netflix.com"));
+            repository.save(new Movie(3L, "Pelicula 3", "Comedia", "www.netflix.com"));
+            repository.save(new Movie(4L, "Pelicula 4", "Ficcion", "www.netflix.com"));
         };
     }
 }
